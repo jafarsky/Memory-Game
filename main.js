0896f1d4e6b729ab932893fb5b0ae694a5ab8d4c
Sky <jafarsky@users.noreply.github.com>
2022-10-13 20:05:50 +0300

    js

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
 const allCards = document.querySelectorAll('.memory-card');
 const allBackFaces = document.querySelectorAll('.back-face');
 
+let lockCards = false;
 let hasShowcard = true;
 let firstCard, secondCard;
 
-async function hideCards()
+
+function removeClickEvent()
 {
-    await new Promise((resolve, reject) => {
-        setTimeout( _ => {
-            firstCard.querySelector('.back-face').classList.add('hidden');
-            firstCard.querySelector('.front-face').classList.remove('hidden');
-            secondCard.querySelector('.back-face').classList.add('hidden');
-            secondCard.querySelector('.front-face').classList.remove('hidden');
-        }, 333);
-    });
+    firstCard.removeEventListener('click', clickCard);
+    secondCard.removeEventListener('click', clickCard);
 }
 
+/********************************* hide back-face ,and show front face  */
+function hideBack_showFront()
+{
+    lockCards = true; // we are now locked all cards from click
+
+    setTimeout( _ => {
+        firstCard.querySelector('.back-face').classList.add('hidden');
+        firstCard.querySelector('.front-face').classList.remove('hidden');
+        secondCard.querySelector('.back-face').classList.add('hidden');
+        secondCard.querySelector('.front-face').classList.remove('hidden');
+
+        lockCards = false // and now oppen all cards to click
+    }, 1111);
+} //____________________________________________________________________ hideBack_showFront()
+
+function isMatch()
+{
+    const is_match = (firstCard.dataset.framework === secondCard.dataset.framework);
+
+    is_match  ?   removeClickEvent()  :  hideBack_showFront();
+}
+
+/****************** when we click on card */
 function clickCard()
 {
+    /***  lock all cards  ***/
+    if (lockCards)    return;
+    
+
     this.querySelector('.front-face').classList.add('hidden');
     this.querySelector('.back-face').classList.remove('hidden');
 
@@ -25,35 +48,23 @@ function clickCard()
         // First click
         firstCard = this;
         hasShowcard = false;
-
     }
-    else {
+    else
+    {
         // Second Click
         secondCard = this;
         hasShowcard = true;
         
-        console.log(firstCard.dataset.framework)
-        console.log(secondCard.dataset.framework)
-
-        //--------------------------------------------- if cards match :)
-        if (firstCard.dataset.framework === secondCard.dataset.framework)
-        {
-            firstCard.removeEventListener('click', clickCard);
-            secondCard.removeEventListener('click', clickCard);
-        }
-        // if Not :/
-        else
-        {
-            hideCards();
-        }
+        isMatch();
     }
-}
+} //_____________________ clickCard()
 
-/****************************************** Add class 'hidden' for all img.back-face , to be able remove class 'hidden
-for (let i = 0 ; i < allCards.length ; i++)
-    allCards[i].querySelector('.back-face').classList.add('hidden'); */
 
+/* 
+ * Add class 'hidden' for all img.back-face , to be able remove class 'hidden 👇🏻
+ */
 allBackFaces.forEach (backface => backface.classList.add('hidden'));
-
+/*
+ * Add Click Event for All Cards Didv 👇🏻
+ */
 allCards.forEach (card => card.addEventListener('click', clickCard));
-
