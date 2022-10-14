1e6775f17592b220dcf71583e85fffaf345ae508
Sky <jafarsky@users.noreply.github.com>
2022-10-14 20:45:14 +0300

    add msg when player win

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let allBackFaces = document.querySelectorAll('.back-face');
 let nextLevelBtn = document.getElementById('nextLevelBtn');
 let allCurrentLevel = document.querySelectorAll('.currentLevel');
 let scoreInDocument = document.getElementById('score');
+let scoreContent = document.getElementById('scoreContent');
+let levelContent = document.getElementById('levelContent');
+let whenSheWin = document.getElementById('whenSheWin');
 
 let lockCards = false;
 let hasShowcard = true;
@@ -13,27 +16,28 @@ let secondCard = 0;
 let score = 0;
 let levelsCount = 1;
 
+// here below const array to store names of back-face img to send it later to the newcard() fun. 
+const backFaceImgName1 = ['sameroom', 'nuxt-icon', 'github-octocat', 'waffle-icon'];
+const backFaceImgName2 = ['nightwatch', 'mailchimp-freddie', 'phalcon', 'testmunk'];
 
-const backFaceImgName1 = ['sameroom', 'nuxt-icon', 'github-octocat', 'waffle-icon', 'nightwatch', 'mailchimp-freddie', 'phalcon', 'testmunk']
-const backFaceImgName2 = ['nightwatch', 'mailchimp-freddie', 'phalcon', 'testmunk']
 
-
-function newCards(name)
+/********************* default mode of game */
+function defaultMode()
 {
-    let a = `
-                <!--------------------------------- ${name} -->
-                <div class="memory-card" data-framework="${name}">
-                <img class="front-face" src="https://api.iconify.design/logos:sinatra.svg" alt="sinatra" />
-                <img class="back-face" src="https://api.iconify.design/logos:${name}.svg" alt="${name}" />
-                </div>
-                <div class="memory-card" data-framework="${name}">
-                <img class="front-face" src="https://api.iconify.design/logos:sinatra.svg" alt="sinatra" />
-                <img class="back-face" src="https://api.iconify.design/logos:${name}.svg" alt="${name}" />
-                </div>
-            `;
-    return a;
-}
+    shuffle();
+    score = 0;
+    scoreInDocument.innerText = score;
 
+    // remove class 'hidden' for all img.front-face , to make mode default 👇🏻
+    allFrontFaces.forEach (frontFace => frontFace.classList.remove('hidden'));
+
+    // Add class 'hidden' for all img.back-face , to be able remove class 'hidden : 👇🏻
+    allBackFaces.forEach (backface => backface.classList.add('hidden'));
+
+    // Add Click Event for All Cards Didv : 👇🏻
+    allCards.forEach (card => card.addEventListener('click', clickCard));
+}
+defaultMode();
 
 
 /****************** shuffling cards places */
@@ -84,39 +88,6 @@ function isMatch()
 } //_____________ isMatch()
 
 
-nextLevelBtn.onclick = _ => {
-
-    nextLevelBtn.classList.add('hidden');
-
-    setTimeout( _ => {
-
-        let card1 = backFaceImgName1[levelsCount - 2];
-        let card2 = backFaceImgName2[levelsCount - 2];
-
-        memoryGameContainer.innerHTML += newCards(card1) + newCards(card2);
-
-        allCurrentLevel.forEach (currentLevel => currentLevel.innerText = levelsCount);
-
-        if (levelsCount == 2) {
-            memoryGameContainer.classList.add('grid-cols-3');
-        }
-        else if (levelsCount <= 4) {
-            memoryGameContainer.classList.add('grid-cols-4');
-        }
-        else {
-            memoryGameContainer.classList.add('grid-cols-5');
-        }
-        
-        
-            
-        
-        updateVariable();
-        defaultMode();
-    }, 1111);
-}
-
-
-
 /************************* remove click() events from first & second cards */
 function removeClickEvent()
 {
@@ -130,36 +101,18 @@ function removeClickEvent()
 
         allCurrentLevel[1].innerText = levelsCount;
 
-        nextLevelBtn.classList.remove('hidden');
+        if (levelsCount <= 5) {
+            nextLevelBtn.classList.remove('hidden');
+        }
+        else {
+            levelContent.classList.add('hidden');
+            scoreContent.classList.add('hidden');
+            memoryGameContainer.classList.add('hidden');
+            whenSheWin.classList.remove('hidden');
+        }
     }
     scoreInDocument.innerText = score;
 } //_____________ removeClickEvent()
-
-
-function defaultMode()
-{
-    shuffle();
-    score = 0;
-    scoreInDocument.innerText = score;
-
-    // remove class 'hidden' for all img.front-face , to make mode default 👇🏻
-    allFrontFaces.forEach (frontFace => frontFace.classList.remove('hidden'));
-
-    // Add class 'hidden' for all img.back-face , to be able remove class 'hidden : 👇🏻
-    allBackFaces.forEach (backface => backface.classList.add('hidden'));
-
-    // Add Click Event for All Cards Didv : 👇🏻
-    allCards.forEach (card => card.addEventListener('click', clickCard));
-}
-defaultMode();
-
-
-function updateVariable()
-{
-    allCards = document.querySelectorAll('.memory-card');
-    allBackFaces = document.querySelectorAll('.back-face');
-    allFrontFaces = document.querySelectorAll('.front-face');
-}
 
 
 /********************************* hide back-face ,and show front face  */
@@ -180,3 +133,61 @@ function hideBack_showFront()
     }, 1111);
 } //____________________________________________________________________ hideBack_showFront()
 
+
+/********************** to add cards in document */
+function newCards(name)
+{
+    let a = `
+                <!--------------------------------- ${name} -->
+                <div class="memory-card" data-framework="${name}">
+                <img class="front-face" src="https://api.iconify.design/logos:sinatra.svg" alt="sinatra" />
+                <img class="back-face" src="https://api.iconify.design/logos:${name}.svg" alt="${name}" />
+                </div>
+                <div class="memory-card" data-framework="${name}">
+                <img class="front-face" src="https://api.iconify.design/logos:sinatra.svg" alt="sinatra" />
+                <img class="back-face" src="https://api.iconify.design/logos:${name}.svg" alt="${name}" />
+                </div>
+            `;
+    return a;
+} //___________________ newCards(name)
+
+
+/**************************** on click in next level button */
+nextLevelBtn.onclick = _ => {
+
+    setTimeout( _ => {
+
+        // get name of back-face img from our arrays.
+        let card1 = backFaceImgName1[levelsCount - 2];
+        let card2 = backFaceImgName2[levelsCount - 2];
+
+        // add block code of cards in document.
+        memoryGameContainer.innerHTML += newCards(card1) + newCards(card2);
+
+        allCurrentLevel.forEach (currentLevel => currentLevel.innerText = levelsCount);
+
+        if (levelsCount == 2) {
+            memoryGameContainer.classList.add('grid-cols-3');
+        }
+        else if (levelsCount <= 4) {
+            memoryGameContainer.classList.add('grid-cols-4');
+        }
+        else {
+            memoryGameContainer.classList.add('grid-cols-5');
+        }
+        // hide next level btn again.
+        nextLevelBtn.classList.add('hidden');
+        
+        updateVariable();
+        defaultMode();
+    }, 1111);
+} //_____________________________ nextLevelBtn.onclick()
+
+
+/************************ update allCards, allBackFaces and allFrontFaces */
+function updateVariable()
+{
+    allCards = document.querySelectorAll('.memory-card');
+    allBackFaces = document.querySelectorAll('.back-face');
+    allFrontFaces = document.querySelectorAll('.front-face');
+} //_____________________________ updateVariable()
